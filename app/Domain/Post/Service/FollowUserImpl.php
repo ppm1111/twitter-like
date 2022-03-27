bf295a29e8bd9b73cb4941cd44090b4711503492
ppm1111 <ppqqbbdd1111@gmail.com>
2022-03-27 23:46:51 +0800

    移除不用的repo

--- app/Domain/Post/Service/FollowUserImpl.php
+++ app/Domain/Post/Service/FollowUserImpl.php
@@ -4,21 +4,15 @@ namespace App\Domain\Post\Service;
 
 use App\Domain\Post\Service\Contract\FollowUser;
 use App\Domain\Post\Repository\UserRepository;
-use App\Domain\Post\Repository\AuthRepository;
 use App\Exceptions\ForbidenException;
 
 class FollowUserImpl implements FollowUser
 {
     private $userRepo;
-    private $authRepo;
 
-    public function __construct(
-        UserRepository $userRepo,
-        AuthRepository $authRepo
-        )
+    public function __construct(UserRepository $userRepo)
     {
         $this->userRepo = $userRepo;
-        $this->authRepo = $authRepo;
     }
 
     public function follow($id, $userId)
